413ca67f206c0f81577c8a4569143b43b5e480fb
ramesh <ramesh_rn@yahoo.co.in>
2020-08-17 12:24:02 -0400

    Test case update

---
+++ employeeservFunctionalTests/src/main/java/com/paypal/bfs/test/employeeserv/EmployeeApplicationIntegrationTests.java
@@ -0,0 +1,34 @@
+package com.paypal.bfs.test.employeeserv;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(
+		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+		classes = EmployeeservApplication.class
+)
+@AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:import.sql")
+public class EmployeeApplicationIntegrationTests {
+	@Autowired
+	MockMvc mockMvc;
+
+	@Test
+	public void testGetEmployeeByID() throws Exception {
+		MvcResult mvcResult = mockMvc.perform(
+				MockMvcRequestBuilders.get("/v1/bfs/employees/100").accept(MediaType.APPLICATION_JSON)
+		).andReturn();
+		System.out.println(mvcResult.getResponse().getContentAsString());
+	}
+
+}
---
+++ employeeservFunctionalTests/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+
+insert into Address(id, line1, line2, city, state, country, zip) values (1, '15711 Clems', '', 'charlotte', 'NC', 'USA', 28277);
+insert into Employee(id, first_name, last_name, date_of_birth, address_id ) values (100, 'Json', 'Bear', sysdate, 1);
